9e7caa8a131fcda4e41dad232f50b33f9f75fcd5
minji10 <ch991012@naver.com>
2025-01-22 18:46:20 +0900

    Day_1

---
+++ Day_1/숫자형.py
@@ -0,0 +1,21 @@
+num1 = 10
+num2 = -3
+num3 = 0
+print(num1)
+print(num2)
+print(num3)
+
+print(num1 - num2)
+
+num1 = 100
+num3 = 20
+print(num1 - num3)
+
+# int()
+print(int(1.9))
+print(int('100'))
+
+print(int(True))
+print(int(False))
+
+print(int(True + 10))
---
+++ Day_1/신상정보.py
@@ -0,0 +1,16 @@
+name = '권민지' # '싱글 라인' => 한줄 문자열을 저장
+age = 20
+address = '''
+우편 번호 112233
+서울특별시 강남구 신사동
+신사아파트 5008호
+''' # '''멀티플 라인''' => 여러줄 문자열을 저장
+
+weight = 50
+height = 180
+
+print(name)
+print(age)
+print(address)
+print(weight)
+print(height)
